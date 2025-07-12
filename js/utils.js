c00de9da58eca6dae55aac310142b2d7fed15fb0
Valerian <mr.onixel@gmail.com>
2025-07-12 23:32:17 +0500

    Догружаю модальное окно рендер, переключаю работу thumbnails в мейн джс
    плюс микро правки по самому коду

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
 // Рандомайзер для чисел в диапазоне
-const getRandomInteger = (min = 0, max = 10, stepAfterDot = 0) => {
+export const getRandomInteger = (min = 0, max = 10, stepAfterDot = 0) => {
   if ((min < 0) || (max < 0) || (max === min)) {
     window.console.warn('Диапозон должен быть положительным и состоять минимум из 1 цифры');
     return 0;
@@ -10,6 +10,33 @@ const getRandomInteger = (min = 0, max = 10, stepAfterDot = 0) => {
   }
   return Math.round(Math.random() * (max - min) + min);
 };
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-export {getRandomInteger, getRandomArrayElement};
+// Функци для получения шаблона
+export const findTemplate = (id) => {
+  const template = document.querySelector(`#${id}`);
+  if (!template) {
+    throw new Error(`Template not found: ${id}`);
+  }
+  if (!(template instanceof HTMLTemplateElement)) {
+    throw new Error(`Template id not a template: ${id}`);
+  }
+  return template.content.firstElementChild;
+};
+
+// Функци для отрисовки шаблона
+export const renderPack = (items, makeElement, container) => {
+  const fragment = document.createDocumentFragment();
+  items.forEach((item) => {
+    fragment.appendChild(makeElement(item));
+  });
+  container.appendChild(fragment);
+};
+
+export const clearPack = (element) => {
+  element.innerHTML = '';
+};
+
+export const isEscapeKey = (evt) => evt.key === 'Escape';
+
+
